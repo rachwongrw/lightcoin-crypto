979627e2ebae85c5f6fb24395da3b5aad4af4897
Rachel Wong <rwrachelwong@gmail.com>
2018-11-20 23:10:16 +0000

    Implement inheritance with Transaction class

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ class Account {
   }
 }
 
-class Withdrawal {
+class Transaction {
   constructor(amount, account) {
     this.amount = amount;
     this.account = account;
   }
-
   commit() {
-    this.account.balance -= this.amount;
+    this.account.balance += this.value;
   }
-
 }
 
-class Deposit {
-  constructor(amount, account) {
-    this.amount = amount;
-    this.account = account;
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
   }
-  commit() {
-    this.account.balance += this.amount;
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
   }
 }
 
@@ -35,16 +35,27 @@ class Deposit {
 
 const myAccount = new Account('crypto');
 
-t1 = new Withdrawal(50.25, myAccount); // passing myAccount into this object is Dependency Injection!
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
+console.log('Closing Balance:', myAccount.balance);
 
-console.log('Balance:', myAccount.balance);
+
+// t1 = new Withdrawal(50.25, myAccount); // passing myAccount into this object is Dependency Injection!
+// t1.commit();
+// console.log('Transaction 1:', t1);
+
+// t2 = new Withdrawal(9.99, myAccount);
+// t2.commit();
+// console.log('Transaction 2:', t2);
+
+// t3 = new Deposit(120.00, myAccount);
+// t3.commit();
+// console.log('Transaction 3:', t3);
+
+// console.log('Balance:', myAccount.balance);
