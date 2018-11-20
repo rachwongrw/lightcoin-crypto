301f63069d0443abb97edcb6bd0d4b22463f8d85
Rachel Wong <rwrachelwong@gmail.com>
2018-11-20 22:55:22 +0000

    Add account class - dependency injection

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0; // makes sense to start balance at 0.
+  }
+}
 
-  constructor(amount) {
+class Withdrawal {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
 
 }
 
-
+class Deposit {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    this.account.balance += this.amount;
+  }
+}
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('crypto');
+
+t1 = new Withdrawal(50.25, myAccount); // passing myAccount into this object is Dependency Injection!
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
